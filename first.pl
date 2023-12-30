8444b83684f865a1527a04a654b2778c3f26bc9f
Dalpatsinghbhayal <113092138+Dalpatsinghbhayal@users.noreply.github.com>
2023-12-30 22:17:15 +0530

    adding new file

---
+++ first.pl
@@ -0,0 +1,11 @@
+valuable(gold).
+male(dalpat).
+owner(dalpat,gold).
+father(dalpat,honey).
+gives(dalpat,book,honey).
+
+likes(john,flowers).
+likes(john,chicks).
+likes(john,mangoes).
+likes(john,oranges).
+likes(john,study).
